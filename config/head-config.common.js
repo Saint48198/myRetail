eb80fb146be5e0112ea636ef1ba030dfb972494a
sdaniels <daniels.scott.r@gmail.com>
2017-04-29 16:31:42 -0400

    More clean up
    Removed unneeded files and references to AngularClass.

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -37,9 +37,5 @@ module.exports = {
     /** <link> tags for a Web App Manifest **/
     { rel: 'manifest', href: '/assets/manifest.json' }
   ],
-  meta: [
-    { name: 'msapplication-TileColor', content: '#00bcd4' },
-    { name: 'msapplication-TileImage', content: '/assets/icon/ms-icon-144x144.png', '=content': true },
-    { name: 'theme-color', content: '#00bcd4' }
-  ]
+  meta: []
 };
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,43 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'product',
+  styles: [`
+  `],
+  template: `
+    <h1>About</h1>
+    <div>
+      For hot module reloading run
+      <pre>npm run start:hmr</pre>
+    </div>
+    <div>
+      <h3>
+        patrick@AngularClass.com
+      </h3>
+    </div>
+    <pre>this.localState = {{ localState | json }}</pre>
+  `
+})
+export class ProductComponent implements OnInit {
+
+  public localState: any;
+  constructor(
+    public route: ActivatedRoute
+  ) {}
+
+  public ngOnInit() {
+    this.route
+      .data
+      .subscribe((data: any) => {
+        // your resolved data from route
+        this.localState = data.yourData;
+      });
+
+    console.log('hello `About` component');
+  }
+
+}
