226e58df3ef9fb8e1acb2a6cf70328348aa7426a
sdaniels <daniels.scott.r@gmail.com>
2017-05-06 12:04:38 -0400

    Created default styles for buttons and updated the product actions layout and style.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -13,6 +13,8 @@ export class ProductComponent implements OnInit {
 
     public product: Product = new Product();
     public images: Object[] = [];
+    public returnMessage: string = '';
+
 
     constructor(
         private _route: ActivatedRoute,
@@ -20,15 +22,26 @@ export class ProductComponent implements OnInit {
     ) {}
 
     public ngOnInit() {
-        this._route.params.switchMap((params: Params) => {
+        this._route.params.switchMap((params: Params) => { // could be used in future to get product id from url
             return this._productSvc.getProduct();
         }).subscribe((resp: Product[]) => {
             if (resp.length === 1) {
                 this.product = resp[0];
-
                 this.images = this.product.Images[0]['PrimaryImage'].concat(this.product.Images[0]['AlternateImages']);
+
+                if (this.product.ReturnPolicy && this.product.ReturnPolicy.length) {
+                    let returnPolicy = this.product.ReturnPolicy[0]['ReturnPolicyDetails'] ?
+                        this.product.ReturnPolicy[0]['ReturnPolicyDetails'][0] : {};
+
+                    this.returnMessage =  this.createReturnMsg(returnPolicy['policyDays'], returnPolicy['guestMessage']);
+                }
             }
         });
     }
 
+    public createReturnMsg (days, guestMessage): string {
+        return `This item must be returned within ${days} days of the ship date.
+                                <a [routerLink]="'policy'">${guestMessage}</a>. Prices, promotions, styles 
+                                and availability may vary by store and online.`;
+    }
 }
