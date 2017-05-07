fb5136ddbfb3c216410a91df7ebf4d0f97291b93
sdaniels <daniels.scott.r@gmail.com>
2017-05-07 14:27:30 -0400

    updated the styles for the image slider

--- src/app/_shared/image-viewer/image-viewer.component.ts
+++ src/app/_shared/image-viewer/image-viewer.component.ts
@@ -12,7 +12,22 @@ export class ImageViewerComponent {
     @Input() set images (images: Object[]) {
 
         this.imageCollection = images || [];
+
+        this.imageCollection.forEach((image, index) => {
+            if (index === 0) {
+                image['active'] = true;
+            } else {
+                image['active'] = false;
+            }
+        });
+
+        console.log(this.imageCollection);
+
         this.currentImage = this.imageCollection.length ? this.imageCollection[0] : {};
     };
+
+    public selectImage (e) {
+
+    }
 
 }
